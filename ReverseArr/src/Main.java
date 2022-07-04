4dbfe2636ecf772e9acb7efa736a49a0bf6f072c
FieBee <phuonglf98@gmail.com>
2022-07-04 21:29:20 +0700

    add reverseArr

---
+++ ReverseArr/src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.println("Enter a size");
+            size = scanner.nextInt();
+            if (size > 20 ){
+                System.out.println("Size does not exceed 20");
+            }
+        }while (size > 20);
+        array = new int[size];
+        int i = 0;
+
+        while (i< array.length){
+            System.out.println("Nhap phan tu "+(i+1)+" cua mang");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.println("\"%-20s%s\", \"Elements in array: \", \"\"");
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j]+ "\t");
+        }
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+        System.out.printf("\n%-20s%s", "Reverse array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+    }
+}
