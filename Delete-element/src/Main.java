64e477fbe5e41990f082f4c66991d7607781a1ae
FieBee <phuonglf98@gmail.com>
2022-07-05 15:22:02 +0700

    edit Add-element part2

--- Delete-element/src/Main.java
+++ Delete-element/src/Main.java
@@ -12,10 +12,12 @@ public class Main {
         for (int i = 0; i<arr.length; i++,indexDel++){
             if (arr[i] == x){
                 System.out.println(" Vi tri phan tu xoa la: " + indexDel);
-                while (i<arr.length-1){
-                    arr[i] = arr[i+1];
-                    i++;
+                int j = i;
+                while (j<arr.length-1){
+                    arr[j] = arr[j+1];
+                    j++;
                 }
+                i--;
             }
         }
         for (int element : arr){
