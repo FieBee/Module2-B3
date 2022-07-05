8c3b827910d1c3e3580f31b4a3f4e13b0541a80e
FieBee <phuonglf98@gmail.com>
2022-07-05 15:17:27 +0700

    edit Add-element

--- Add-element/src/Main.java
+++ Add-element/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int arr[] = {1,2,3,4,5,6,7,0,0};
         System.out.println("Nhgap so can chen");
         int x = input.nextInt();
         System.out.println("Nhap vi tri can chen vao trong mang");
@@ -11,8 +11,14 @@ public class Main {
         if (index <= -1 || index >= arr.length-1){
             System.out.println("Ko chen dc");
         }
-        arr[index] = x;
-
+        for (int i = 0; i < arr.length; i++) {
+            if (i==index){
+                for (int j = 0; j < arr.length-index ; j++) {
+                    arr[arr.length-j-1] = arr[arr.length-j-2];
+                }
+                arr[index] =x;
+            }
+        }
         for (int element: arr){
             System.out.println(element);
         }
